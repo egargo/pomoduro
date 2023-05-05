f02704b42c315f7433d4253414b20a91b746eadd
Clint Egargo <clintegargo@protonmail.ch>
2023-05-06 00:09:13 +0800

    perf: lessen call of DOM by storing it in a variable and changing the logic to calculate minutes and seconds

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,4 +1,6 @@
-const pomoDuroNotifyCheck = () => {
+// Check if notification is allowed, otherwise ask the user to allow the
+// notifications.
+const pomoduroNotifyCheck = () => {
     if (Notification.permission === 'granted') {
         console.log('Notifications: enabled');
     } else {
@@ -10,79 +12,66 @@ const pomoDuroNotifyCheck = () => {
     }
 };
 
-const pomoDuroSoundNotify = () => {
-    new Audio('res/audio/notify.mp3').play();
-};
+const pomoduroSoundNotify = () => new Audio('res/audio/notify.mp3').play();
 
-const pomoDuroStartNotify = () => {
+const pomoduroNotifyMessage = (notifyMessage) => {
     new Notification('POMODURO', {
         icon: 'res/icons/pomoDuro.png',
-        body: 'Start',
+        body: notifyMessage,
     });
-    pomoDuroSoundNotify();
 };
 
-const pomoDuroStopNotify = (time) => {
-    new Notification('POMODURO', {
-        icon: 'res/icons/pomoDuro.png',
-        body: 'End',
-    });
-    pomoDuroSoundNotify();
+const pomoduroSendNotify = (notifyMessage) => {
+    pomoduroNotifyMessage(notifyMessage);
+    pomoduroSoundNotify();
 };
 
-const pomodoroWorkTime = localStorage.getItem('study') || '25';
-const pomodoroBreakTime = localStorage.getItem('break') || '5';
+// If the 'study' and 'break' in localStorage does not exist, set its value to
+// '25' and '5', respectively.
+const pomoduroStudyTime = localStorage.getItem('study') || '25';
+const pomoduroBreakTime = localStorage.getItem('break') || '5';
 
-document.getElementById('timer').innerText =
-    pomodoroWorkTime < 10 ? pomodoroWorkTime + ':00' : pomodoroWorkTime + ':00';
+let pomoduroTimer = document.getElementById('pomoduroTimer');
+let timerControlButton = document.getElementById('timerControlButton');
 
-let intervalID, countDown;
+pomoduroTimer.innerText =
+    pomoduroStudyTime < 10
+        ? pomoduroStudyTime + ':00'
+        : pomoduroStudyTime + ':00';
 
-const pomoDuroReset = () => {
-    countDown = pomodoroWorkTime * 60;
+let intervalID, countDown, minute;
+
+const pomoduroReset = () => {
+    countDown = pomoduroStudyTime * 60;
     document.title = 'POMODURO';
-    document.getElementById('timer').innerText = pomodoroWorkTime + ':00';
-};
-
-const updateButton = () => {
-    document.getElementById('timerControlButton').value = 'START';
-    document.getElementById('timerControlButton').onclick = () => {
-        pomoDuroStartTimer();
-    };
-    document.getElementById('timerControlButton').style.boxShadow =
-        '#d6d6d6 0px 6px 0px';
+    pomoduroTimer.innerText = pomoduroStudyTime + ':00';
 };
 
 // Start pomoDuro timer.
 const pomoDuroStartTimer = () => {
-    // document.getElementById('pomodoro-break')
-    pomoDuroStartNotify();
-    document.getElementById('timerControlButton').value = 'PAUSE';
-    document.getElementById('timerControlButton').style.boxShadow =
-        '#ffffff 0px 6px 0px';
-    document.getElementById('timerControlButton').onclick = () => {
+    pomoduroSendNotify('Start');
+    timerControlButton.value = 'PAUSE';
+    timerControlButton.style.boxShadow = '#ffffff 0px 6px 0px';
+    timerControlButton.onclick = () => {
         pomoDuroPauseTimer();
     };
 
     countDown =
-        document.getElementById('timer').getAttribute('name') === 'study'
-            ? pomodoroWorkTime * 60
-            : pomodoroBreakTime * 60;
+        pomoduroTimer.getAttribute('name') === 'study'
+            ? pomoduroStudyTime * 60
+            : pomoduroBreakTime * 60;
 
-    Math.floor(countDown / 60) < 10
-        ? '0' + Math.floor(countDown / 60)
-        : Math.floor(countDown / 60);
+    console.log(countDown);
 
     intervalID = setInterval(() => {
         countDown--;
-        document.title =
-            Math.floor(countDown / 60) + ':' + (countDown % 60) + ' | POMODURO';
-        document.getElementById('timer').innerText =
-            Math.floor(countDown / 60) + ':' + (countDown % 60);
+        minute = (countDown / 60) >> (countDown / 60) % 1;
+        document.title = minute + ':' + (countDown % 60) + ' | POMODURO';
+        pomoduroTimer.innerText = minute + ':' + (countDown % 60);
 
         if (countDown === 0) {
             pomoDuroResetTimer();
-            pomoDuroReset();
+            pomoduroReset();
         }
     }, 1000);
 };
@@ -93,17 +82,25 @@ const pomoDuroPauseTimer = () => {
 };
 
 const pomoDuroResetTimer = () => {
-    pomoDuroSoundNotify();
-    pomoDuroStopNotify();
+    pomoduroSendNotify('Reset');
     updateButton();
     clearInterval(intervalID);
-    pomoDuroReset();
+    pomoduroReset();
 };
 
-const pomodoroSwitchTimerMode = () => {
+const updateButton = () => {
+    // document.getElementById('timerControlButton').value = 'START';
+    timerControlButton.value = 'START';
+    timerControlButton.onclick = () => {
+        pomoDuroStartTimer();
+    };
+    timerControlButton.style.boxShadow = '#d6d6d6 0px 6px 0px';
+};
+
+const pomoduroSwitchTimerMode = () => {
     let pomodoro_study = document.getElementById('pomodoro-study');
     let pomodoro_break = document.getElementById('pomodoro-break');
-    let pomodoro_timer = document.getElementById('timer');
+    let pomodoro_timer = document.getElementById('pomoduroTimer');
 
     pomodoro_study.style = 'background-color: #397097';
 
@@ -111,16 +108,16 @@ const pomodoroSwitchTimerMode = () => {
         pomodoro_study.style = 'background-color: #397097';
         pomodoro_break.style = 'none';
         pomodoro_timer.setAttribute('name', 'study');
-        pomodoro_timer.innerText = pomodoroWorkTime + ':00';
+        pomodoro_timer.innerText = pomoduroStudyTime + ':00';
     });
 
     pomodoro_break.addEventListener('click', () => {
         pomodoro_break.style = 'background-color: #397097';
         pomodoro_study.style = 'none';
         pomodoro_timer.setAttribute('name', 'break');
-        pomodoro_timer.innerText = pomodoroBreakTime + ':00';
+        pomodoro_timer.innerText = pomoduroBreakTime + ':00';
     });
 };
 
-pomodoroSwitchTimerMode();
-pomoDuroNotifyCheck();
+pomoduroSwitchTimerMode();
+pomoduroNotifyCheck();
