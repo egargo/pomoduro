268dcbad6e41ce8edc3096d05be22b046d26be3e
Clint Egargo <clintegargo@protonmail.ch>
2023-05-20 19:56:15 +0800

    feat: add notify.js

---
+++ src/js/notify.js
@@ -0,0 +1,33 @@
+'use-strict';
+
+// Check if notification is allowed, otherwise ask the user to allow the
+// notifications.
+const pomoduroNotifyCheck = () => {
+    if (Notification.permission === 'granted') {
+        console.log('Notifications: enabled');
+    } else {
+        Notification.requestPermission().then((permission) => {
+            if (permission === 'granted') {
+                console.log('Notifications: enabled');
+            }
+        });
+    }
+};
+
+const pomoduroSoundNotify = () => new Audio('res/audio/notify.mp3').play();
+
+const pomoduroNotifyMessage = (notifyMessage) => {
+    new Notification('POMODURO', {
+        // icon: 'res/icons/pomoDuro-white.png',
+        // icon: '🍅',
+        icon: 'res/icons/pomoduro.png',
+        body: notifyMessage,
+    });
+};
+
+export const pomoduroSendNotify = (notifyMessage) => {
+    pomoduroNotifyMessage(notifyMessage);
+    pomoduroSoundNotify();
+};
+
+pomoduroNotifyCheck();
